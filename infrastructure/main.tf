02894479e7cc547f0ec8487f7d45dbca1dddc9da
zeeket <emil.heliste@helsinki.fi>
2024-05-04 23:12:44 +0300

    fmt, push apply requires fmt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,20 +4,20 @@ resource "digitalocean_ssh_key" "terraform" {
 }
 
 resource "digitalocean_droplet" "www-1" {
-  image = "ubuntu-20-04-x64"
-  name = "www-2"
+  image  = "ubuntu-20-04-x64"
+  name   = "www-2"
   region = "fra1"
-  size = "s-1vcpu-1gb"
+  size   = "s-1vcpu-1gb"
   ssh_keys = [
     resource.digitalocean_ssh_key.terraform.id
   ]
 
   connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
     private_key = var.pvt_key
-    timeout = "2m"
+    timeout     = "2m"
   }
 
   provisioner "remote-exec" {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,15 @@
 variable "do_token" {
-    description = "Digital Ocean Token"
-    type = string
-    sensitive = true
+  description = "Digital Ocean Token"
+  type        = string
+  sensitive   = true
 }
 variable "pvt_key" {
-    description = "Private Key"
-    type = string
-    sensitive = true
+  description = "Private Key"
+  type        = string
+  sensitive   = true
 }
 
 variable "pub_key" {
-    description = "Public Key"
-    type = string
+  description = "Public Key"
+  type        = string
 }
