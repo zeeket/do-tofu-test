1ee28085f89183d7594c863b2ea0a47936638bda
zeeket <emil.heliste@helsinki.fi>
2024-05-04 08:25:45 +0300

    test

---
+++ infrastructure/variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {
+    description = "Digital Ocean Token"
+    sensitive = true
+}
+variable "pvt_key" {
+    description = "Private Key"
+    sensitive = true
+}
---
+++ infrastructure/version.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "HYTKY"
+
+    workspaces {
+      name = "default"
+    }
+  }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
